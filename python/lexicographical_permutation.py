62834c8a28f732171ef4e41ed84a74cac2ee836c
Jeet-Majumdar <mail2majumdar@gmail.com>
2023-04-30 22:35:38 +0530

    lexicographical permutation

---
+++ python/lexicographical_permutation.py
@@ -0,0 +1,29 @@
+'''
+Lexicographical Ordering
+
+1. Find largest i such that a[i-1] < a[i]
+2. Find largest j such that a[i-1] < a[j]
+3. Interchange a[i-1] and a[j]
+4. Reverse order of digit a[i], a[i+1], ... a[n]
+
+'''
+
+def LexiPermutation(a):
+    if len(a) < 2:
+        return a[::-1]
+
+    inverse_point = len(a) - 1
+    while inverse_point >= 0 and a[inverse_point-1] < a[inverse_point]:
+        inverse_point = inverse_point - 1 
+    
+    if inverse_point < 0:
+        return []
+    
+    for i in reversed[range(inverse_point, len(a))]:
+        if a[i] > a[inverse_point]:
+            a[i], a[inverse_point] = a[inverse_point], a[i]
+            break
+    
+    a[inverse_point + 1] = reversed(a[inverse_point + 1:])
+
+    return a
