3861a716eed9a24a3647bd7d7da839cfc2738ccb
jeet <mail2majumdar@gmail.com>
2023-12-01 13:06:57 +0530

    quick sort changes: more intuitive partition step

--- python/sort_quick.py
+++ python/sort_quick.py
@@ -13,7 +13,7 @@ Quick Sort
 
 def QuickSort(arr, start, end):
     if start < end:
-        pivot = Partition(arr, start, end)
+        pivot = Partition_intuitive(arr, start, end)
         QuickSort(arr, start, pivot - 1)
         QuickSort(arr, pivot+1, end)
 
@@ -26,6 +26,24 @@ def Partition(arr, start, end):
             i = i + 1
     arr[i], arr[end] = arr[end], arr[i]
     return i
+
+def Partition_intuitive(arr, start, end):
+    # Take a pivot element, and ensure that all the elements to the left of pivot are less than pivot,
+    # and all to the right are greater than pivot
+    # Concept from: https://www.youtube.com/watch?v=7h1s2SojIRw
+    
+    pivot = arr[start]
+    i = start
+    j = end
+    while i < j:
+        while arr[i] <= pivot:
+            i += 1
+        while arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[j], arr[start] = arr[start], arr[j]
+    return j
 
 if __name__ == '__main__':
     a = [4, 5, 7, 1, 55, 24, 2, 9]
