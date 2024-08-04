f6c28ed32a6b5e9947e3eb16375e46c50ce4f6ed
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-08-04 23:53:24 +0530

    update best time to buy sell stock

--- LeetCode/array_best_time_to_buy_sell_stock_leetcode_121.py
+++ LeetCode/array_best_time_to_buy_sell_stock_leetcode_121.py
@@ -25,3 +25,19 @@ Constraints:
 1 <= prices.length <= 105
 0 <= prices[i] <= 104
 """
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        max_profit = 0
+        for i in range(1, len(prices)):
+    
+            # Checking for lower buy value
+            if (buy > prices[i]):
+                buy = prices[i]
+    
+            # Checking for higher profit
+            elif (prices[i] - buy > max_profit):
+                max_profit = prices[i] - buy
+        return max_profit
+
