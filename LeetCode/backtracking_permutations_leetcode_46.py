9f65c8eef99cf354f07c1afe2445710a1793a6bb
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-08-08 00:58:05 +0530

    update

--- LeetCode/backtracking_permutations_leetcode_46.py
+++ LeetCode/backtracking_permutations_leetcode_46.py
@@ -49,24 +49,3 @@ class Solution:
         
         return res
 
-
-"""
-## Another approach
-
-def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        def backtrack(perm):
-            # base case
-            if len(perm) == len(nums):
-                res.append(perm)  
-                return
-
-            for num in nums:
-                if num not in perm:
-                    backtrack(perm + [num])
-                    
-        backtrack([])
-        return res
-
-"""
