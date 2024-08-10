c2cf0ee8eb689668665b185f2cf5c21fa6dca1ac
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-08-10 12:35:22 +0530

    alternative solution of permutations

--- LeetCode/backtracking_permutations_leetcode_46.py
+++ LeetCode/backtracking_permutations_leetcode_46.py
@@ -49,3 +49,30 @@ class Solution:
         
         return res
 
+
+"""
+## Alternative 
+## Time O(n!)
+## Space O(n)
+
+# https://www.youtube.com/watch?v=gFm1lEfnzUQ
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        ans, sol = [], []
+
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+                return 
+            
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans
+        
+
+"""
