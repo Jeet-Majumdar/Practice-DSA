d07a8b038b0a316ef566c2a105f61c10f5bbd831
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-08-11 00:35:53 +0530

    update stack_largest_rectangle_in_histogram_leetcode_84

--- LeetCode/stack_largest_rectangle_in_histogram_leetcode_84.py
+++ LeetCode/stack_largest_rectangle_in_histogram_leetcode_84.py
@@ -17,3 +17,24 @@ Constraints:
 0 <= heights[i] <= 10^4
 """
 
+from typing import List 
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        ## https://www.youtube.com/watch?v=zx5Sw9130L0
+        ## Monotonous Stack (Max) + Revisit remaining stack elements
+        
+        max_area = 0
+        stack = []
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                max_area = max(max_area, height * (i - index))
+                start = index
+            stack.append((start, h))
+        
+        for i, h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
+        return max_area
