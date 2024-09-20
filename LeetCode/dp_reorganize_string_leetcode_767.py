4a2a9a72d3a6b32a7ca18d8846bd267599e59a79
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-09-21 01:00:15 +0530

    dp_reorganize_string_leetcode_767.py

---
+++ LeetCode/dp_reorganize_string_leetcode_767.py
@@ -0,0 +1,53 @@
+"""
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+Return any possible rearrangement of s or return "" if not possible.
+
+Example 1:
+Input: s = "aab"
+Output: "aba"
+
+Example 2:
+Input: s = "aaab"
+Output: ""
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+"""
+
+from collections import Counter
+import heapq
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # Ref Video: https://www.youtube.com/watch?v=2g_b1aYTHeg
+        
+        count = Counter(s) # Hashmap, count each char
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
+
+        heapq.heapify(maxHeap) # O(n)
+
+        prev = None
+        res = ""
+
+        while maxHeap or prev:
+
+            if prev and not maxHeap:
+                return ""
+
+            # most frequent, except prev
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            
+            if cnt != 0:
+                prev = [cnt, char]
+        
+        return res
