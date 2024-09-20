2919fd58c775776291916ec0ef9e0f395bde7bd5
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-09-21 00:34:21 +0530

    correction

--- LeetCode/dp_longest_univalue_path_leetcode_687.py
+++ LeetCode/dp_longest_univalue_path_leetcode_687.py
@@ -35,27 +35,18 @@ from typing import Optional
 class Solution:
     def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
         ## Ref Video: https://www.youtube.com/watch?v=cwYVhyl2A1s
-
-        self.res = 0
-
-        def dfs(node):
-            if node is None:
+        # Time: O(n)
+        # Space: O(n)
+        longest = [0]
+        def traverse(node):
+            if not node:
                 return 0
-            
-            leftChild = dfs(node.left)
-            rightChild = dfs(node.right)
-            if node.left and node.val == node.left.val:
-                leftChild = leftChild + 1
-            if node.right and node.val == node.right.val:
-                rightChild = rightChild + 1
-            
-            nodeVal = leftChild + rightChild
-            self.res = max(self.res, nodeVal)
-            
-            # Return the max chain length from this node
-            return max(leftChild, rightChild)
-        
-        dfs(root)
-        return self.res
+            left_len, right_len = traverse(node.left), traverse(node.right)
+            left = (left_len + 1) if node.left and node.left.val == node.val else 0
+            right = (right_len + 1) if node.right and node.right.val == node.val else 0
+            longest[0] = max(longest[0], left + right)
+            return max(left, right)
+        traverse(root)
+        return longest[0]
             
         
